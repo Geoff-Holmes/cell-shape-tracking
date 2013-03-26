4ad2095a1c3b21762619112acfb877ff7760bd8d
Geoff Holmes <geoffrholmes@gmail.com>
2013-03-26 13:19:04 +0000

    add whole track plot

--- matlab/@grhCell/cellPlot.m
+++ matlab/@grhCell/cellPlot.m
@@ -1,5 +1,11 @@
 function cellPlot(obj, i)
 
 % plot the cell outline at time i using basis Bspline
-
-plot(obj.Bspline.curve(obj.ctrlPts{i}))
+if nargin > 1
+    plot(obj.Bspline.curve(obj.ctrlPts{i}))
+else
+    hold on
+    for i = 1:length(obj.Bspline)
+        plot(obj.Bspline.curve(obj.ctrlPts{i}))
+    end
+end
