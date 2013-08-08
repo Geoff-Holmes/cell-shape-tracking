d27aa7a6d5160f3e6416f0231df8b4937278b448
Geoff Holmes <geoffrholmes@gmail.com>
2013-08-08 09:57:58 +0100

    more consitency in animations

--- @grhManager/animateBoundaryVelocities.m
+++ @grhManager/animateBoundaryVelocities.m
@@ -1,7 +1,11 @@
-function animateTracks(obj, tracks)
+function animateBoundaryVelocities(obj, tracks)
 
-% animate trajectory for cell/track IDs specified as list in tracks
+% animate boundary velocities for cell/track IDs specified as list in tracks
 % if no list specified show all tracks
+
+if obj.Bspline.L < 3
+    display('WARNING: Bspline dimension less than 3 may cause problems with velocities');
+end
 
 % check whether a subset of tracks is specified
 if nargin == 1
@@ -27,11 +31,11 @@ endFrame   = max([obj.cells(tracks).lastSeen]);
     
 % loop for repeating option
 for s = 1:10
-for t = startFrame:endFrame
+for t = startFrame:endFrame-1
     
     
-    imshow(double(~obj.Data{t}) + .9*double(~~obj.Data{t}));
-    hold on
+%     imshow(double(~obj.Data{t}) + .9*double(~~obj.Data{t}));
+%     hold on
     
     for j = 1:length(tracks)
         
@@ -43,16 +47,40 @@ for t = startFrame:endFrame
         end
         
         if thisCell.firstSeen <= t && thisCell.lastSeen >= t
-            p(j) = plot(thisCell, t-thisCell.firstSeen+1);
+            
+            % get correct time index for this cell
+            tThis = t-thisCell.firstSeen+1;
+            % plot current cell outline
+            p(j) = plot(thisCell.snake(tThis));
             set(p(j), 'color', cMap(cInd(j),:));
-            axis([0 obj.DataXYlims(1) 0 obj.DataXYlims(2)]);
-            c = 10*round(thisCell.centroid(t-thisCell.firstSeen+1)/10);
+            hold on
+            % calculate velocities from current states
+            C = thisCell.Cmatrix{tThis};
+            state = thisCell.states{tThis};
+            v = C * state(obj.Bspline.L+1:end);
+            % get points on boundary where velocites have been calculated
+            c = thisCell.snake(tThis).curve(-length(v));
+            v = v(1:end-1);
+            % plot velocity vectors
+            grhCline(c, c+v);
+            % plot cell position at next time frame if available
+            if thisCell.lastSeen > t
+                try
+                    plot(thisCell.snake(tThis+1), 'r'); 
+                catch ex
+                    ex
+                end
+            end
+            % label the cells
+            c = 10*round(thisCell.centroid(tThis)/10);
             txt(j) = text(real(c), imag(c)+50, num2str(tracks(j)));
             set(txt(j), 'color', cMap(cInd(j),:));
-%             hold on
         end
     end
     title(['Frame : ' num2str(t)])
+    if nargin == 1
+        axis([0 obj.DataXYlims(1) 0 obj.DataXYlims(2)]);
+    end
     hold off
     % give option to pause between frames
     if s ==1 && t == 1
--- workingExample.m
+++ workingExample.m
@@ -17,27 +17,27 @@ data = 'neutroImages_hiRes';
 % v = 5;
 
 % for random walk model with velocity estimation
-% dt = 1;
-% A = sparse([eye(B.L) dt*eye(B.L); zeros(B.L) zeros(B.L)]);
-% C = [];
-% W = sparse(10*eye(2*B.L));
-% v = 5;
-
-% for constant velocity model
 dt = 1;
-% state update matrix
-A = sparse([eye(B.L) dt*eye(B.L); zeros(B.L) eye(B.L)]);
-% observation matrix empty since time varying and evaluated each time
-C = []; 
-% some correlation between states
-% but what about correlation between velocities and positions
-% Q = 10*sparse(toeplitz([2 1 zeros(1, B.L-3) 1]));
-Q = eye(B.L);
-% see Bar-Shalom Estimation with Appln to Tracking ... (2001) p218
-G = sparse([dt^2/2*eye(B.L); dt*eye(B.L)]);
-W = G * Q * G';
-% observation noise level
+A = sparse([eye(B.L) dt*eye(B.L); zeros(B.L) zeros(B.L)]);
+C = [];
+W = sparse(10*eye(2*B.L));
 v = 5;
+
+% % for constant velocity model
+% dt = 1;
+% % state update matrix
+% A = sparse([eye(B.L) dt*eye(B.L); zeros(B.L) eye(B.L)]);
+% % observation matrix empty since time varying and evaluated each time
+% C = []; 
+% % some correlation between states
+% % but what about correlation between velocities and positions
+% % Q = 10*sparse(toeplitz([2 1 zeros(1, B.L-3) 1]));
+% Q = eye(B.L);
+% % see Bar-Shalom Estimation with Appln to Tracking ... (2001) p218
+% G = sparse([dt^2/2*eye(B.L); dt*eye(B.L)]);
+% W = G * Q * G';
+% % observation noise level
+% v = 5;
 
 % construct dynamic model
 M = grhModel(A, C, W, v);
