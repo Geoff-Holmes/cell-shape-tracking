4440fae01d20759356f1197f7ad49c2cad17ea94
Geoff Holmes <geoffrholmes@gmail.com>
2013-08-14 13:05:33 +0100

    added warning

--- @grhBspline/getCentroid.m
+++ @grhBspline/getCentroid.m
@@ -2,6 +2,10 @@ function c = getCentroid(obj, Q)
 
 % get centroid of shape described by spline curve with control points Q
 % this centroid follows the simple calculation of Blake p 63
+% NB this is not invariant to reparamatrisation and in a particular there a
+% discrepancy between the centroid calcuated this way and the the centroid
+% of the original fitted boundary data
+
 if isa(Q, 'grhSnake')
     Q = Q.ctrlPts;
 end
