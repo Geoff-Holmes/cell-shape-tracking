30bab79d26ed3ecd10b65822dc446aa2cbc5dbd8
Geoff Holmes <geoffrholmes@gmail.com>
2013-08-14 14:32:23 +0100

    use own centroid calculation

--- @grhImageHandler/getFrame.m
+++ @grhImageHandler/getFrame.m
@@ -1,28 +1,35 @@
 function frame = getFrame(obj, A)
-    % get all cell boundaries and centroids above threshold area
-    if issparse(A)
-        A = full(A);
-    end
-    % get connected objects
-    B = bwconncomp(A, 4);
-    concomp = regionprops(B, 'centroid', 'PixelIdxList', 'Area');
-    % initialise reconstructed image
-    A = zeros(size(A));
-    % count number of admissable objects found
-    cc = 0;
-    for i = 1 :length(concomp)
-        temp = concomp(i);
-        if temp.Area > obj.minAreaThresh
-            % add object to reconstructed binary image for boundary finding
-            A(temp.PixelIdxList) = 1;
-            cc = cc+1;
-            centroids(cc) = temp.Centroid * [1;1i];
-        end
-    end
-    % get boundary
-    [neutroBoundi, L] = bwboundaries(A,4,'noholes');
-    for k = 1:cc
-        bounds{k} = neutroBoundi{k}*[1i;1];
-    end
-    frame = grhFrame(bounds, centroids);
+    
+% frame = getFrame(obj, A)
+%
+% get all cell boundaries and centroids above threshold area
+% inbuilt centroid conflicts with Bspline centroid whereas mean of boundary
+% seems to match it
+
+if issparse(A)
+    A = full(A);
 end
+% get connected objects
+B = bwconncomp(A, 4);
+%     concomp = regionprops(B, 'centroid', 'PixelIdxList', 'Area');
+concomp = regionprops(B, 'PixelIdxList', 'Area');
+% initialise reconstructed image
+A = zeros(size(A));
+% count number of admissable objects found
+cc = 0;
+for i = 1 :length(concomp)
+    temp = concomp(i);
+    if temp.Area > obj.minAreaThresh
+        % add object to reconstructed binary image for boundary finding
+        A(temp.PixelIdxList) = 1;
+        cc = cc+1;
+%             centroids(cc) = temp.Centroid * [1;1i];
+    end
+end
+% get boundary
+[neutroBoundi, L] = bwboundaries(A,4,'noholes');
+for k = 1:cc
+    bounds{k} = neutroBoundi{k}*[1i;1];
+    centroids(k) = mean(bounds{k});
+end
+frame = grhFrame(bounds, centroids);
